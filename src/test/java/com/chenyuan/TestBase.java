5fb20debaf2dcefa8569cb0826787e0525b56cab
chenyuan <chenyuan@test.com>
2023-04-12 18:36:45 +0800

    我的api自动化测试

--- src/test/java/com/chenyuan/TestBase.java
+++ src/test/java/com/chenyuan/TestBase.java
@@ -1,4 +1,13 @@
 package com.chenyuan;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TestBase {
+    public List readExcleData(String exclePathArr,String [] sheetNameArr){
+        //未完成
+        List allExcelData = new ArrayList();// excel文件數組
+
+        return allExcelData;
+    }
 }
