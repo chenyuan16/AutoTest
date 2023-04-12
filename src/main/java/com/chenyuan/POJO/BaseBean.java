c4e1570888f75cd5e2aa858a08ff0b1085d072f4
chenyuan <chenyuan@test.com>
2023-04-12 17:21:06 +0800

    我的api自动化测试

--- src/main/java/com/chenyuan/POJO/BaseBean.java
+++ src/main/java/com/chenyuan/POJO/BaseBean.java
@@ -1,4 +1,30 @@
 package com.chenyuan.POJO;
 
 public class BaseBean {
+    private String excelName;
+    private String sheetName;
+
+    public String getExcelName() {
+        return excelName;
+    }
+
+    public void setExcelName(String excelName) {
+        this.excelName = excelName;
+    }
+
+    public String getSheetName() {
+        return sheetName;
+    }
+
+    public void setSheetName(String sheetName) {
+        this.sheetName = sheetName;
+    }
+
+    @Override
+    public String toString() {
+        return "BaseBean{" +
+                "excelName='" + excelName + '\'' +
+                ", sheetName='" + sheetName + '\'' +
+                '}';
+    }
 }
